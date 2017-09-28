6d498072a451222d42236a07feab07b4433e6a08
alexandriaware <ali.mercer92@gmail.com>
2017-09-27 20:23:17 -0400

    Problems 5-6

--- ConditionalPracticeProblems/Program.cs
+++ ConditionalPracticeProblems/Program.cs
@@ -61,23 +61,65 @@ namespace ConditionalPracticeProblems
 
 
 
-            //Problem 4
-            Console.WriteLine("Please enter a letter");
-            char letter = char.Parse(Console.ReadLine());
+            ////Problem 4
+            //Console.WriteLine("Please enter a letter");
+            //char letter = char.Parse(Console.ReadLine());
 
-            switch (letter)
-            {
-                case 'a':
-                case 'e':
-                case 'i':
-                case 'o':
-                case 'u':
-                    Console.WriteLine("That letter is a vowel.");
-                    break;
-                default:
-                    Console.WriteLine("That letter is a consonant.");
-                    break;
-            }
+            //switch (letter)
+            //{
+            //    case 'a':
+            //    case 'e':
+            //    case 'i':
+            //    case 'o':
+            //    case 'u':
+            //        Console.WriteLine("That letter is a vowel.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("That letter is a consonant.");
+            //        break;
+            //}
+
+
+
+            ////Problem 5
+            //Console.WriteLine("Please input a number");
+            //int firstNumber = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Please input a second number");
+            //int secondNumber = int.Parse(Console.ReadLine());
+
+            //if (firstNumber > secondNumber)
+            //{
+            //    Console.WriteLine(firstNumber + " is greater.");
+            //}
+            //else if (firstNumber == secondNumber)
+            //{
+            //    Console.WriteLine("The numbers are equal.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(secondNumber + " is greater.");
+            //}
+
+
+
+            //Problem 6
+            Console.WriteLine("Please input a number");
+            double firstNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please input a second number");
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please input a third number");
+            double thirdNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please input a fourth number");
+            double fourthNumber = double.Parse(Console.ReadLine());
+
+            double avg = firstNumber + secondNumber + thirdNumber + fourthNumber;
+            double sum = avg / 4;
+
+            Console.WriteLine("The mode is " + sum);
 
 
 
